23c6326a7ec881b697c7f767210d5bd509cddcff
Nathanael Cunningham <penguin2774@gmail.com>
2015-05-07 04:18:51 -0400

    Added a testing namespace for use in repl

---
+++ src/treemud/testing.clj
@@ -0,0 +1,14 @@
+(ns treemud.testing
+  (:use clojure.pprint)
+  (:require [treemud.server :as server]
+            [treemud.world :as world]
+            [treemud.world.init :as init]
+            [treemud.event :as event]
+            [treemud.npc :as npc]
+            [treemud.tick :as tick]
+            [treemud.event :as event]
+            ))
+
+
+
+
